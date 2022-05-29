9286ac1fec4b1cf61294bb457e57695b5af48459
jpilimon <jakub.pilimon@gmail.com>
2022-05-29 19:10:22 +0200

    initial

---
+++ src/main/java/io/pillopl/bik/BikApplication.java
@@ -0,0 +1,13 @@
+package io.pillopl.bik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pillopl/bik/BikApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pillopl.bik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
